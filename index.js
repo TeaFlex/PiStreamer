b7ced3466e49742d2b62f9052e88d25c80d366be
teaflex <nicolas.jeuniaux.pro@hotmail.com>
2020-11-06 22:01:57 +0100

    Streaming working

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app)
-const io = require('socket.io')(http);
 const port = 8070;
-const {spawn} = require('child_process');
+const Stream_server = require('./lib/stream_server');
+const stream = new Stream_server(http,{fps:24});
 
 app.set('view engine', 'ejs');
 app.use(express.static('static_files'));
@@ -11,13 +11,6 @@ app.use(express.static('static_files'));
 app.get("/", (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.render("index");
-});
-
-io.of('/').on('connect', (socket) => {
-    console.log("Someone has appeared !");
-    socket.on('startStream', ()=>{
-        var stream = spawn('raspivid', ['--nopreview', '-w', '640', '-h', '480', '-fps', '12', ''])
-    });
 });
 
 http.listen(port, () =>{
