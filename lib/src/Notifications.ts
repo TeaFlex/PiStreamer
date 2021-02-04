f15d05cf2ebfcb057bd81badbf509be24d1be1e1
teaflex <teaflex.dev@hotmail.com>
2021-02-04 16:36:40 +0100

    typescript time !!!

---
+++ lib/src/Notifications.ts
@@ -0,0 +1,10 @@
+import winston, { Logger } from 'winston';
+
+export var logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      new winston.transports.Console()
+    ]
+});
